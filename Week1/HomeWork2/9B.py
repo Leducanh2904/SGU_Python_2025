2ed068a0ebf63be1205ae56cce4eb1bd433555fb
Leducanh2904 <ldanhsguk22@gmail.com>
2025-09-16 09:37:23 +0700

    update 9B

--- Week1/HomeWork2/9B.py
+++ Week1/HomeWork2/9B.py
@@ -3,11 +3,10 @@
 def fast_power(a, b):
     if b == 0:
         return 1
-    elif b % 2 == 0:
-        half = fast_power(a, b // 2)
+    half = fast_power(a, b // 2)
+    if b % 2 == 0:
         return half * half
     else:
-        half = fast_power(a, (b - 1) // 2)
         return half * half * a
 
 # Nhập a và b từ bàn phím
